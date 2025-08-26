f7ffb8fefe8d1e11bc2c3f0d230c31677503be11
Pedro Vieira <pedro.hipolito1203@gmail.com>
2025-08-26 10:58:47 -0300

    Bugs Corrigidos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component'; // Corrigido aqui
 
-describe('App', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent], // Corrigido aqui
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent); // Corrigido aqui
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gestor-loja-angular');
+  it(`should have the 'LuPA' title`, () => { // Teste atualizado para o nome do app
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.appName).toEqual('LuPA');
   });
 });
